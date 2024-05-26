197ba156c08006b4e3b8b63e96e9dd40525e9614
PAMINAIOT <161015038+PAMINAIOT@users.noreply.github.com>
2024-05-26 16:22:16 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -709,12 +709,12 @@ namespace grove {
         })*/
         sendAtCmd("AT");
         sendAtCmd("AT+CIPCLOSE");
-        let output = ""
+        /*let output = ""
        
         sendAtCmd("AT+CIPSTART=\"TCP\",\"" + "paminasogo.ddns.net" + "\",9090");
         result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000);
         output = result.toString();
-        basic.showString(output);
+        basic.showString(output);*/
         /*sendAtCmd("AT+CIPMODE=1");
         function sendToThingsBoard(temperature: number) {
             const url = 'http://paminasogo.ddns.net:9090/api/v1/wV0EikPcEMHcE3u3zvgI/telemetry';
